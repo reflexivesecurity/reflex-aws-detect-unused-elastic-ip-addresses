a75d90da4f467141ca23c7b1613bd693af01171e
rjulian <richard@rjulian.net>
2020-09-21 00:31:01 -0700

    Fix for event payload parsing

--- source/detect_unused_eips.py
+++ source/detect_unused_eips.py
@@ -25,10 +25,10 @@ class UnusedEIPRule(AWSRule):
 
     def any_detached_eips(self):
         """ Returns True if the bucket is encrypted, False otherwise """
-        all_addresses = self.client.describe_addresses()['Addresses']
+        all_addresses = self.client.describe_addresses()["Addresses"]
         for address in all_addresses:
-            if 'AssociationId' not in address.keys():
-                self.unattached_eips.append(address['AllocationId'])
+            if "AssociationId" not in address.keys():
+                self.unattached_eips.append(address["AllocationId"])
         if self.unattached_eips:
             return True
         return False
@@ -41,8 +41,9 @@ class UnusedEIPRule(AWSRule):
 def lambda_handler(event, _):
     """ Handles the incoming event """
     print(event)
-    if subscription_confirmation.is_subscription_confirmation(event):
-        subscription_confirmation.confirm_subscription(event)
+    event_payload = json.loads(event["Records"][0]["body"])
+    if subscription_confirmation.is_subscription_confirmation(event_payload):
+        subscription_confirmation.confirm_subscription(event_payload)
         return
-    unused_eip_rule = UnusedEIPRule(json.loads(event["Records"][0]["body"]))
+    unused_eip_rule = UnusedEIPRule(event_payload)
     unused_eip_rule.run_compliance_rule()
